e676b9baff14816ebfadc3bc01e9df33dfa74c99
Kena Wakwoya <kwakwoya@gmail.com>
2023-06-10 11:16:11 +0300

    add steps to follow to run the app

--- routes/chat.router.js
+++ routes/chat.router.js
@@ -2,26 +2,26 @@ const express = require('express');
 const router = express.Router();
 const chatController = require('../controller/chat.controller');
 
-
+// Route: get all messages 
 router.get('/', (req, res) => {
    const {sender,reciever} = req.body;
    chatController.getChatMessage(sender,reciever).then(data => res.json(data));
 });
 
 
-// Route: Create a new profile
+// Route: Create a new message and emit to reciever
 router.post('/',(req, res) => {
    console.log("body", req.body);
    const {sender,reciever,content} = req.body;
    chatController.handleMessage(sender,reciever,content).then(data => res.json(data));
 });
 
-// Route: Update a profile
+// Route: Update a message
 router.put('/:id', (req, res) => {
    chatController.updateChatMessage(req.params.id, req.body).then(data => res.json(data));
 });
 
-// Route: Delete a specific profile
+// Route: Delete a specific message
 router.delete('/:id', (req, res) => {
    chatController.deleteChatMessage(req.params.id).then(data => res.json(data));
 });
@@ -29,7 +29,7 @@ router.delete('/:id', (req, res) => {
 
 
 
-router.get('/message', (req, res) => {
+router.get('/', (req, res) => {
    chatController.getChats().then(data => res.json(data))
 
 })
